dc431bbd41afe1bbca8c5d5078a37bce87fa4799
Anmol varma <anmol_varma@github.com>
2018-03-04 00:51:03 +0530

    Some of the other fields also checked for JOI validation

--- tests/router/addUserToAadhaarDB.test.js
+++ tests/router/addUserToAadhaarDB.test.js
@@ -142,4 +142,134 @@ describe('Testing the hapi server for GET request', () => {
       done();
     });
   });
+  test('Invalid status code for wrong pincode', (done) => {
+    const options = {
+      method: 'POST',
+      url: '/user',
+      payload: {
+        aadhaarNo: '123412341235',
+        name: 'Anmol varma',
+        dob: '02-09-1989',
+        gender: 'Male',
+        contact: '8976576500',
+        co: 'S/O Varma ji',
+        house: '120/210',
+        street: 'Lajpat Nagar',
+        landmark: 'efgh',
+        locality: 'locality',
+        subDistrict: 'Kanpur nagar',
+        district: 'Kanpur',
+        state: 'Uttar Pradesh',
+        pincode: '20805',
+      },
+    };
+    Server.inject(options, (response) => {
+      expect(response.result.validation.keys[0]).toBe('pincode');
+      done();
+    });
+  });
+  test('Invalid status code for wrong pincode', (done) => {
+    const options = {
+      method: 'POST',
+      url: '/user',
+      payload: {
+        aadhaarNo: '123412341235',
+        name: 'Anmol varma',
+        dob: '02-09-1989',
+        gender: 'Male',
+        contact: '8976576500',
+        co: 'S/O Varma ji',
+        house: '120/210',
+        street: 'Lajpat Nagar',
+        landmark: 'efgh',
+        locality: 'locality',
+        subDistrict: 'Kanpur nagar',
+        district: 'Kanpur',
+        state: 'Uttar Pradesh',
+        pincode: '20805',
+      },
+    };
+    Server.inject(options, (response) => {
+      expect(response.result.statusCode).toBe(400);
+      done();
+    });
+  });
+  test('Invalid status code for wrong contact number', (done) => {
+    const options = {
+      method: 'POST',
+      url: '/user',
+      payload: {
+        aadhaarNo: '3412341235',
+        name: 'Anmol varma',
+        dob: '02-09-1989',
+        gender: 'Male',
+        contact: '8976asd576500',
+        co: 'S/O Varma ji',
+        house: '120/210',
+        street: 'Lajpat Nagar',
+        landmark: 'efgh',
+        locality: 'locality',
+        subDistrict: 'Kanpur nagar',
+        district: 'Kanpur',
+        state: 'Uttar Pradesh',
+        pincode: '208005',
+      },
+    };
+    Server.inject(options, (response) => {
+      expect(response.result.validation.keys[0]).toBe('aadhaarNo');
+      done();
+    });
+  });
+  test('Invalid status code for wrong contact number', (done) => {
+    const options = {
+      method: 'POST',
+      url: '/user',
+      payload: {
+        aadhaarNo: '3412341235',
+        name: 'Anmol varma',
+        dob: '02-09-1989',
+        gender: 'Male',
+        contact: '897sad6576500',
+        co: 'S/O Varma ji',
+        house: '120/210',
+        street: 'Lajpat Nagar',
+        landmark: 'efgh',
+        locality: 'locality',
+        subDistrict: 'Kanpur nagar',
+        district: 'Kanpur',
+        state: 'Uttar Pradesh',
+        pincode: '208005',
+      },
+    };
+    Server.inject(options, (response) => {
+      expect(response.result.statusCode).toBe(400);
+      done();
+    });
+  });
+  test('Invalid status code for invalid request', (done) => {
+    const options = {
+      method: 'POST',
+      url: '/userss',
+      payload: {
+        aadhaarNo: '3412341235',
+        name: 'Anmol varma',
+        dob: '02-09-1989',
+        gender: 'Male',
+        contact: '8976576500',
+        co: 'S/O Varma ji',
+        house: '120/210',
+        street: 'Lajpat Nagar',
+        landmark: 'efgh',
+        locality: 'locality',
+        subDistrict: 'Kanpur nagar',
+        district: 'Kanpur',
+        state: 'Uttar Pradesh',
+        pincode: '208005',
+      },
+    };
+    Server.inject(options, (response) => {
+      expect(response.result.statusCode).toBe(404);
+      done();
+    });
+  });
 });
