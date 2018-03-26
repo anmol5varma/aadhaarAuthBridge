7f66fd57b6af6cdf5e82e961614843c4154d378c
Anmol varma <anmol_varma@github.com>
2018-03-26 16:33:08 +0530

    Added added email

--- models/user.js
+++ models/user.js
@@ -18,6 +18,7 @@ module.exports = (sequelize, DataTypes) => {
     state: DataTypes.STRING,
     pc: DataTypes.INTEGER,
     po: DataTypes.INTEGER,
+    email: DataTypes.STRING,
   }, {
     classMethods: {
       associate() {
